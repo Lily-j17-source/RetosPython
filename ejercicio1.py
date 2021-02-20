84b0e98cdc56a73504413a3b6ffe96181d5d7159
Lily-j17-source <liliana.1793@hotmail.com>
2021-02-20 12:16:49 -0600

    ADD ejercicio1

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,1 +1,11 @@
+##
+numero = 100
+def largo_de_un_numero(n):
+   # numero= int(input("Dame un numero"))
+   contador = 1
+   while(n /10 > 0):
+        n = n/10
+        contador = contador + 1
+        return(contador)
 
+print(largo_de_un_numero(numero))
